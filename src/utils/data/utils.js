63fbbd3ffaf5f9d0d6e7707d9ac90357eb3df142
Rémy Raes <webmaster@esnlille.fr>
2018-01-30 19:47:30 +0100

    Beginning work on storage

---
+++ src/utils/data/utils.js
@@ -0,0 +1,45 @@
+var STORAGE_UTILS = (function () {
+
+    /**
+      * Stores json object into user browser
+      * @param {String} key name of the data
+      * @param {JSON} json object to store
+      * @returns {boolean} whether the storage succeeded or not
+      * @private
+      **/
+    function store_json(key, json) {
+
+    }
+
+    /**
+      * Gets json object from user browser
+      * @param {String} key name of the data
+      * @returns {JSON} object corresponding to key
+      * @private
+      **/
+    function _get_json(key) {
+
+    }
+
+    /**
+      * Converts a json object to a yaml one
+      * @param {JSON} json object to convert
+      * @returns {YAML} converted object
+      * @private
+      **/
+    function _json_to_yaml(json) {
+
+    }
+
+    /**
+      * Converts a yaml object to a json one
+      * @param {YAML} yaml object to convert
+      * @returns {JSON} converted object
+      * @private
+      **/
+    function _yaml_to_json(yaml) {
+
+    }
+
+
+})(browser);
