c3640ba3c5b28e2ffd5ff43bbf12e7b8317f4e8e
Jules Spicht <jules.spicht@etudiant.univ-lille1.fr>
2018-01-31 13:46:28 +0100

    Fixed storage utils support

--- src/utils/data/utils.js
+++ src/utils/data/utils.js
@@ -1,68 +1,94 @@
 var STORAGE_UTILS = (function () {
 
+    var STORAGE_TEST_KEY = '2971344b-1076-4f31-8818-d67fbf22213f';
+
     /**
-      * Checks if the localStorage API is supported by the browser
-      * @returns {boolean} whether the API is supported or not
-      * @private
-      **/
-    function _localStorage_is_supported() {
-        return (typeof(Storage) !== "undefined");
+     * Checks if the localStorage API is supported by the browser
+     * @returns {boolean} whether the API is supported or not
+     * @private
+     **/
+    function _supported() {
+        var storageAvailable = typeof(Storage) !== "undefined";
+        if (storageAvailable) {
+            try {
+                _set(STORAGE_TEST_KEY, '1');
+                _remove(STORAGE_TEST_KEY);
+            } catch (e) {
+                storageAvailable = false;
+            }
+        }
+        return storageAvailable;
     }
 
     /**
-      * Stores json object into user browser
-      * @param {String} key name of the data
-      * @param {JSON} json object to store
-      * @returns {boolean} whether the storage succeeded or not
-      * @private
-      **/
-    function _store_json(key, json) {
-        if(!_localStorage_is_supported)
-            return;
-        localStorage.setItem(key, JSON.stringify(json));
+     * Stores json object into user browser
+     * @param {String} key name of the data
+     * @param {String} strData string to store
+     * @returns {boolean} whether the storage succeeded or not
+     * @private
+     **/
+    function _set(key, strData) {
+        localStorage.setItem(key, strData);
     }
 
     /**
-      * Gets json object from user browser
-      * @param {String} key name of the data
-      * @returns {JSON} object corresponding to key
-      * @private
-      **/
-    function _get_json(key) {
-        if(!_localStorage_is_supported)
-            return;
+     * Gets json object from user browser
+     * @param {String} key name of the data
+     * @returns {String} string data corresponding to key
+     * @private
+     **/
+    function _get(key) {
         return localStorage.getItem(key);
     }
 
     /**
-      * Converts a json object to a yaml one
-      * @param {JSON} json object to convert
-      * @returns {YAML} converted object
-      * @private
-      **/
+     * Remove given key data
+     * @param key
+     * @private
+     */
+    function _remove(key) {
+        return localStorage.removeItem(key);
+    }
+
+    /**
+     * Converts a json object to a yaml one
+     * @param {JSON} json object to convert
+     * @returns {YAML} converted object
+     * @private
+     **/
     function _json_to_yaml(json) {
 
     }
 
     /**
-      * Converts a yaml object to a json one
-      * @param {YAML} yaml object to convert
-      * @returns {JSON} converted object
-      * @private
-      **/
+     * Converts a yaml object to a json one
+     * @param {YAML} yaml object to convert
+     * @returns {JSON} converted object
+     * @private
+     **/
     function _yaml_to_json(yaml) {
 
     }
 
-    return {
-        store_json: function(key, json){
-            _store_json(key, json);
+    var exports = {
+        store: function (key, json) {
+            return _set(key, json);
         },
+        get: function (key) {
+            return _get(key);
+        }
+    };
 
-        get_json: function(key){
-            return _get_json(key);
+    //Check support
+    if (!_supported()) {
+        for (var k in exports) {
+            exports[k] = function () {
+                return false
+            };
         }
     }
+
+    return exports;
 
 
 })(browser);
