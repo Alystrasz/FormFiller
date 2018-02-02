b4ecc754305ab863908137a6b94ce0ae1d454115
Raes Remy <remy.raes@etudiant.univ-lille1.fr>
2018-02-02 08:10:26 +0100

    Added some JSON input for testing purposes

--- src/ui/popup/popup.js
+++ src/ui/popup/popup.js
@@ -20,6 +20,28 @@ document.addEventListener('DOMContentLoaded', function () {
         MESSAGE_HANDLER.send('CONTENT_SCRIPT', 'get_forms', true);
     }, false);
 
-
+    //Test function to import files
+    document.getElementById('file').addEventListener('change', () => {
+        import_json();
+    });
 });
 
+
+function import_json() {
+    var input = document.getElementById('file');
+    var current_data = input.files.item(0);
+    var content = undefined;
+
+    var file = input.files[0];
+    let reader = new FileReader();
+
+    reader.onload = function () {
+        content = (this.result);
+        _handle_data(JSON.parse(content));
+    };
+    reader.readAsText(current_data);
+}
+
+function _handle_data(obj) {
+    console.log(obj);
+}
